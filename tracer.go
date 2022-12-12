12946b5b9879a9b7ffdd3a864a22d8ddcd86e405
Petar Komordzic <komordzic.sr37.2020@uns.ac.rs>
2022-12-12 01:33:58 +0100

    Debug: pointers

--- tracer.go
+++ tracer.go
@@ -17,7 +17,7 @@ import (
 /*
 InitTracer initializes tracer and returns it. It also returns error which may happen during init process
 */
-func InitTracer(serviceName string) (trace.Tracer, error) {
+func InitTracer(serviceName string) (*trace.Tracer, error) {
 	ctx := context.Background()
 	exp, err := newExporter()
 	if err != nil {
@@ -28,7 +28,7 @@ func InitTracer(serviceName string) (trace.Tracer, error) {
 	otel.SetTracerProvider(tp)
 	tracer := tp.Tracer(serviceName)
 	otel.SetTextMapPropagator(propagation.TraceContext{})
-	return tracer, nil
+	return &tracer, nil
 }
 
 /*
@@ -38,7 +38,6 @@ func newExporter() (*jaeger.Exporter, error) {
 	addr := os.Getenv("JAEGER_ADDRESS")
 	if addr == "" {
 		return nil, errors.New("couldn't read .env variables for JAEGER_ADDRESS. Please check if you provided it correctly")
-
 	}
 	exp, err := jaeger.New(jaeger.WithCollectorEndpoint(jaeger.WithEndpoint(addr)))
 	if err != nil {
